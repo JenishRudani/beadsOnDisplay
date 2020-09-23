660c86493e4754a4d9f93b21f984c43a24c8bb3d
JenishRudani98 <jrudani1999@gmail.com>
2020-09-24 02:04:19 +0530

    Final Code Ready

--- lib/sunflower.dart
+++ lib/sunflower.dart
@@ -1,6 +1,9 @@
 import 'package:beadsOnDisplay/sunflowerPainter.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
+import 'package:supercharged/supercharged.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 
 final TargetPlatform platform = TargetPlatform.linux;
 final Color primaryColor = Colors.red[300];
@@ -13,9 +16,26 @@ class Sunflower extends StatefulWidget {
 }
 
 class _SunflowerState extends State<Sunflower> {
+  CustomAnimationControl control =
+      CustomAnimationControl.PLAY; // <-- state variable
+
   double seeds = 100.0;
 
   int get seedCount => seeds.floor();
+  double duration = 1;
+  void toggleDirection() {
+    setState(() {
+      // toggle between control instructions
+      control = (control == CustomAnimationControl.PLAY)
+          ? CustomAnimationControl.PLAY_REVERSE
+          : CustomAnimationControl.PLAY;
+    });
+  }
+
+  void _setDuration(double newValue) {
+    duration = newValue;
+    print(duration);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,60 +52,167 @@ class _SunflowerState extends State<Sunflower> {
         ),
       ),
       home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[400],
+          title: Text(
+            "Beads Animation",
+            style: TextStyle(
+                color: Colors.black87,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.5,
+                fontSize: 24),
+          ),
+          elevation: 15,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(35),
+            ),
+          ),
+        ),
         backgroundColor: Colors.cyan[100],
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Container(
-              child: SizedBox(
-                width: 551,
-                height: 551,
-                child: CustomPaint(
-                  painter: SunflowerPainter(seedCount),
-                ),
-              ),
-            ),
-            Container(
-              height: 40,
-              width: 200,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(30),
-                ),
-                border: Border.all(width: 10, color: Colors.transparent),
-                color: Colors.cyan[300],
-              ),
-              child: Center(
-                child: Text(
-                  "Showing $seedCount seeds",
-                  style: GoogleFonts.nunitoSans(
-                    textStyle: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                      letterSpacing: 1.1,
+            Center(
+              child: CustomAnimation<double>(
+                control: control, // <-- bind state variable to parameter
+                tween: (0.0).tweenTo(360.0),
+                startPosition: 0,
+                duration: duration.seconds,
+                delay: (duration * 0.3).seconds,
+                builder: (context, child, value) {
+                  return Transform.rotate(
+                    // <-- animation that moves childs from left to right
+                    angle: value,
+                    child: child,
+                  );
+                },
+                animationStatusListener: (AnimationStatus status) {
+                  if (status == AnimationStatus.completed) {
+                    print("Animation completed!");
+                  }
+                },
+                child: GestureDetector(
+                  child: InkWell(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(600),
+                    ),
+                    onTap: () {
+                      toggleDirection();
+                    },
+                    radius: 1,
+                    splashColor: Colors.orange[200],
+                    child: Container(
+                      width: 551,
+                      height: 551,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: CustomPaint(
+                        painter: SunflowerPainter(seedCount),
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-            SizedBox(
-              height: 25,
+            ConstrainedBox(
+              constraints: BoxConstraints.tightFor(width: 600),
+              child: RowSuper(
+                fill: true,
+                children: [
+                  ConstrainedBox(
+                    constraints: BoxConstraints.tightFor(width: 400),
+                    child: Slider.adaptive(
+                      inactiveColor: Colors.cyanAccent[400],
+                      activeColor: Colors.redAccent[100],
+                      min: 1,
+                      max: 3000,
+                      value: seeds,
+                      onChanged: (newValue) {
+                        setState(() {
+                          seeds = newValue;
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30),
+                      ),
+                      border: Border.all(width: 10, color: Colors.transparent),
+                      color: Colors.cyan[300],
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Showing $seedCount seeds",
+                        style: GoogleFonts.nunitoSans(
+                          textStyle: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                            letterSpacing: 1.1,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
             ConstrainedBox(
-              constraints: BoxConstraints.tightFor(width: 300),
-              child: Slider.adaptive(
-                inactiveColor: Colors.cyanAccent[400],
-                activeColor: Colors.redAccent[100],
-                min: 1,
-                max: 3000,
-                value: seeds,
-                onChanged: (newValue) {
-                  setState(() {
-                    seeds = newValue;
-                  });
-                },
+              constraints: BoxConstraints.tightFor(width: 600),
+              child: RowSuper(
+                fill: true,
+                children: [
+                  ConstrainedBox(
+                    constraints: BoxConstraints.tightFor(width: 400),
+                    child: Slider.adaptive(
+                      inactiveColor: Colors.cyanAccent[400],
+                      activeColor: Colors.deepOrangeAccent[200],
+                      min: 0,
+                      max: 3,
+                      divisions: 10,
+                      value: duration.toDouble(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          _setDuration(newValue);
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30),
+                      ),
+                      border: Border.all(width: 10, color: Colors.transparent),
+                      color: Colors.cyan[300],
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Animation Speed : " +
+                            duration.toStringAsFixed(2) +
+                            " Seconds",
+                        style: GoogleFonts.nunitoSans(
+                          textStyle: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                            letterSpacing: 1.1,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
--- lib/sunflowerPainter.dart
+++ lib/sunflowerPainter.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
 final TargetPlatform platform = TargetPlatform.linux;
-final Color primaryColor = Colors.black;
+final Color primaryColor = Colors.red;
 
 class SunflowerPainter extends CustomPainter {
-  static const seedRadius = 2.2;
-  static const scaleFactor = 5;
+  double seedRadius = 2.2;
+  static const scaleFactor = 4;
   static const tau = math.pi * 2;
 
   static final phi = (math.sqrt(5) + 1) / 2;
@@ -17,11 +17,32 @@ class SunflowerPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    int scale;
+    if (seeds < 500) {
+      scale = 10;
+      seedRadius = 4;
+    } else if (seeds < 1000) {
+      scale = 8;
+      seedRadius = 4;
+    } else if (seeds < 1500) {
+      scale = 7;
+      seedRadius = 4;
+    } else if (seeds < 2000) {
+      scale = 6;
+      seedRadius = 3;
+    } else if (seeds < 2500) {
+      scale = 5;
+      seedRadius = 2.5;
+    } else if (seeds <= 3000) {
+      scale = 5;
+      seedRadius = 2.3;
+    }
+
     var center = size.width / 2;
 
     for (var i = 0; i < seeds; i++) {
       var theta = i * tau / phi;
-      var r = math.sqrt(i) * scaleFactor;
+      var r = math.sqrt(i) * scale;
       var x = center + r * math.cos(theta);
       var y = center - r * math.sin(theta);
       var offset = Offset(x, y);
@@ -40,7 +61,7 @@ class SunflowerPainter extends CustomPainter {
   // Draw a small circle representing a seed centered at (x,y).
   void drawSeed(Canvas canvas, num x, num y) {
     var paint = Paint()
-      ..strokeWidth = 0.5
+      ..strokeWidth = 2
       ..style = PaintingStyle.fill
       ..color = primaryColor;
     canvas.drawCircle(Offset(x, y), seedRadius, paint);
